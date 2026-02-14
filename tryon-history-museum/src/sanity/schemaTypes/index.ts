484ee2edbacf5f87b174618581793091655a8fb5
Heather Brady <quebelleza@gmail.com>
2026-02-14 14:10:04 -0500

    Add first schema

--- tryon-history-museum/src/sanity/schemaTypes/index.ts
+++ tryon-history-museum/src/sanity/schemaTypes/index.ts
@@ -1,5 +1,5 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import {page} from './page'
 
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+export const schema = {
+  types: [page],
 }
---
+++ tryon-history-museum/src/sanity/schemaTypes/page.ts
@@ -0,0 +1,25 @@
+import {defineType, defineField} from 'sanity'
+
+export const page = defineType({
+  name: 'page',
+  title: 'Page',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {source: 'title'},
+    }),
+    defineField({
+      name: 'content',
+      title: 'Content',
+      type: 'text',
+    }),
+  ],
+})
