81f27051666e5635c06c1e39a63ded0d6db26344
Heather Brady <quebelleza@gmail.com>
2026-07-21 19:42:11 -0400

    Add Microsoft Clarity (xq5mm4wwzy) via next/script

--- tryon-history-museum/src/app/layout.js
+++ tryon-history-museum/src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const viewport = {
   width: "device-width",
@@ -42,6 +43,13 @@ export default function RootLayout({ children }) {
           Skip to main content
         </a>
         {children}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window,document,"clarity","script","xq5mm4wwzy");`}
+        </Script>
       </body>
     </html>
   );
